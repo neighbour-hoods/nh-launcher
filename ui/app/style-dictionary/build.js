b3e29d09cfe86bd4d009eecf2bd9ff2d23d29a3a
Nicholas Stebbings <NICKOLARSE@RISEUP.NET>
2023-05-22 09:33:54 +0100

    get base styles to build

--- ui/app/style-dictionary/build.js
+++ ui/app/style-dictionary/build.js
@@ -6,55 +6,65 @@ const StyleDictionary = require('style-dictionary');
 // that is installed as a dependency of this package.
 registerTransforms(StyleDictionary);
 
-const baseStyles = ['colors', 'radii', 'shadows', 'dimension', 'typography'];
-baseStyles.forEach(group => {
+const groups = ['base'];//, 'semantic', 'themes'];
+groups.forEach(group => {
   StyleDictionary.extend({
-    // include: [`tokens/default/**/*.json`],
-    source: [`./tokens.json`],
-    transform: {
-      // Now we can use the transform 'myTransform' below
-      myTransform: {
-        type: 'name',
-        transformer: (token) => token.path.join('_').toUpperCase()
-      }
-    },
+    // include: [`**/*.json`],
+    include: [`**/${group}/*.json`],
+    // transform: {
+    //   // Now we can use the transform 'myTransform' below
+    //   myTransform: {
+    //     type: 'name',
+    //     transformer: (token) => token.path.join('_').toUpperCase()
+    //   }
+    // },
+    "platforms": {
+      "css": {
+          "transformGroup": "tokens-studio",
+          "buildPath": "../dist/assets/css/",
+          "files": [
+            {
+              "destination": `${group}.css`,
+              "format": "css/variables"
+            }
+          ]
+        },
+        "scss": {
+          "transformGroup": "scss",
+          "prefix": "sd",
+          "buildPath": "dist/assets/scss/",
+          "files": [
+            {
+              "destination": `${group}.scss`,
+              "format": "scss/variables",
+              "options": {
+                "outputReferences": true
+              }
+            }
+          ]
+        }
+      },
   }).buildAllPlatforms();
 });
 
-const myStyleDictionary = StyleDictionary.extend({
-    "source": ["**/tokens.json"],
-    "platforms": {
-        "css": {
-            "transformGroup": "tokens-studio",
-            "buildPath": "../dist/assets/scss/",
-            "files": [
-              {
-                "filter": function(token) {
-                
-                return token.type = "base/colors"
-                },
-                "destination": "variables.css",
-                "format": "css/variables"
-              }
-            ]
-          },
-    //   "scss": {
-    //     "transformGroup": "tokens-studio",
-    //     "prefix": "sd",
-    //     "buildPath": "../dist/assets/scss/",
-    //     "files": [{
-    //       "filter": function(token) {
+// const myStyleDictionary = StyleDictionary.extend({
+//     "source": ["**/tokens.json"],
+//     //   "scss": {
+//     //     "transformGroup": "tokens-studio",
+//     //     "prefix": "sd",
+//     //     "buildPath": "../dist/assets/scss/",
+//     //     "files": [{
+//     //       "filter": function(token) {
             
-    //         return token["base/colors"]
-    //       },
-    //       "destination": "_variables.scss",
-    //       "format": "scss/variables",
-    //       "options": {
-    //         "outputReferences": true
-    //       }
-    //     }]
-    //   }
-    }
-  });
+//     //         return token["base/colors"]
+//     //       },
+//     //       "destination": "_variables.scss",
+//     //       "format": "scss/variables",
+//     //       "options": {
+//     //         "outputReferences": true
+//     //       }
+//     //     }]
+//     //   }
+//   });
 
-myStyleDictionary.buildAllPlatforms();
+// myStyleDictionary.buildAllPlatforms();
