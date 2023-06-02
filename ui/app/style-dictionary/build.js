94e1d759a7c097db815a4154fef349e77acd1cc8
Nicholas Stebbings <NICKOLARSE@RISEUP.NET>
2023-06-02 10:44:42 +0100

    working style dict

--- ui/app/style-dictionary/build.js
+++ ui/app/style-dictionary/build.js
@@ -1,70 +1,124 @@
-const { registerTransforms } = require('@tokens-studio/sd-transforms');
-const StyleDictionary = require('style-dictionary');
+const fs = require("fs")
+const { registerTransforms } = require('@tokens-studio/sd-transforms')
+const StyleDictionary = require('style-dictionary')
 
+registerTransforms(StyleDictionary)
 
-// will register them on StyleDictionary object
-// that is installed as a dependency of this package.
-registerTransforms(StyleDictionary);
+const log = (obj) => console.dir(obj, { depth: null })
 
-const groups = ['base'];//, 'semantic', 'themes'];
-groups.forEach(group => {
-  StyleDictionary.extend({
-    // include: [`**/*.json`],
-    include: [`**/${group}/*.json`],
-    // transform: {
-    //   // Now we can use the transform 'myTransform' below
-    //   myTransform: {
-    //     type: 'name',
-    //     transformer: (token) => token.path.join('_').toUpperCase()
-    //   }
-    // },
-    "platforms": {
-      "css": {
-          "transformGroup": "tokens-studio",
-          "buildPath": "../dist/assets/css/",
-          "files": [
-            {
-              "destination": `${group}.css`,
-              "format": "css/variables"
+const tokenFile = "./style-dictionary/tokens/tokens.json"
+
+const kebabCase = str => str
+    .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
+    .join('-')
+    .toLowerCase();
+
+function MergeRecursive(obj1, obj2) {
+  for (var p in obj2) {
+    try {
+      // Property in destination object set; update its value.
+      if ( obj2[p].constructor==Object ) {
+        obj1[p] = MergeRecursive(obj1[p], obj2[p]);
+      } else {
+        obj1[p] = obj2[p];
+      }
+    } catch(e) {
+      // Property in destination object not set; create it and set its value.
+      obj1[p] = obj2[p];
+    }
+  }
+  return obj1;
+}
+
+function writeThemeTokenFiles(tokenFile) {
+  const filestream = fs.readFileSync(tokenFile)
+  const tokenConfig = JSON.parse(filestream)
+  const themeNames = tokenConfig.$themes.map((theme) => theme.name)
+  const tokenSetOrder = tokenConfig.$metadata.tokenSetOrder
+
+  log(`Processing ${tokenFile} with token set order:`)
+  log(tokenSetOrder)
+
+  for (let themeName of themeNames) {
+    const themeTokenSets = tokenConfig.$themes.find((c) => c.name == themeName).selectedTokenSets
+    log(themeName)
+    log(themeTokenSets)
+
+    const theme = {}
+    tokenSetOrder.forEach(
+      (tokenset) => {
+        console.log(`Processing ${tokenset}...`)
+        const enabled = themeTokenSets[tokenset]
+        if (enabled && enabled[tokenset] !== 'disabled') {
+          // add it
+          for (let [key, value] of Object.entries(tokenConfig[tokenset])) {
+            log(key)
+            if (theme[key]) {
+              theme[key] = MergeRecursive(theme[key], value)
+            } else {
+              theme[key] = value;
             }
-          ]
-        },
-        "scss": {
-          "transformGroup": "scss",
-          "prefix": "sd",
-          "buildPath": "dist/assets/scss/",
-          "files": [
-            {
-              "destination": `${group}.scss`,
-              "format": "scss/variables",
-              "options": {
-                "outputReferences": true
-              }
-            }
-          ]
+          }
         }
+      }
+    );
+    const tokenJSON = JSON.stringify(theme)
+    fs.writeFileSync(`${kebabCase(themeName)}.json`, tokenJSON);
+  }
+  return themeNames.map(kebabCase);
+}
+
+const themeNames = writeThemeTokenFiles(tokenFile);
+
+themeNames.forEach((themeName) => {
+  const sd = StyleDictionary.extend({
+    source: [`${themeName}.json`],
+    platforms: {
+      js: {
+        transformGroup: 'tokens-studio',
+        prefix: "nh",
+        buildPath: `build/${themeName}/js/`,
+        "files": [
+          {
+            "format": "javascript/module",
+            "destination": "variables.js"
+          },
+          {
+            "format": "typescript/module-declarations",
+            "destination": "variables.d.ts"
+          }
+        ]
       },
-  }).buildAllPlatforms();
-});
-
-// const myStyleDictionary = StyleDictionary.extend({
-//     "source": ["**/tokens.json"],
-//     //   "scss": {
-//     //     "transformGroup": "tokens-studio",
-//     //     "prefix": "sd",
-//     //     "buildPath": "../dist/assets/scss/",
-//     //     "files": [{
-//     //       "filter": function(token) {
-            
-//     //         return token["base/colors"]
-//     //       },
-//     //       "destination": "_variables.scss",
-//     //       "format": "scss/variables",
-//     //       "options": {
-//     //         "outputReferences": true
-//     //       }
-//     //     }]
-//     //   }
-//   });
-
-// myStyleDictionary.buildAllPlatforms();
+      css: {
+        transforms: [
+          'ts/descriptionToComment',
+          'ts/size/px',
+          'ts/opacity',
+          'ts/size/lineheight',
+          'ts/type/fontWeight',
+          'ts/resolveMath',
+          'ts/size/css/letterspacing',
+          'ts/typography/css/shorthand',
+          'ts/border/css/shorthand',
+          'ts/shadow/css/shorthand',
+          'ts/color/css/hexrgba',
+          'ts/color/modifiers',
+          'name/cti/kebab',
+        ],
+        prefix: "nh",
+        buildPath: `build/${themeName}/css/`,
+        files: [
+          {
+            destination: 'variables.css',
+            format: 'css/variables',
+            options: {
+              "outputReferences": true
+            }
+          },
+        ],
+      },
+    },
+  });
+  sd.cleanAllPlatforms();
+  sd.buildAllPlatforms();
+})
--- ui/app/style-dictionary/split_tokens.js
+++ ui/app/style-dictionary/split_tokens.js
@@ -1,41 +1,24 @@
 const fs = require('fs');
 const path = require('path');
 
-function splitJSONFile(filePath) {
-    // Read the JSON file
-    const jsonData = fs.readFileSync(filePath, 'utf-8');
-    const data = JSON.parse(jsonData);
+const tokenFile = "tokens.json"
+const filestream = fs.readFileSync(tokenFile)
+const tokenConfig = JSON.parse(filestream);
 
-    // Create the 'tokens' directory if it doesn't exist
-    const tokensDir = path.join(__dirname, 'tokens');
-    if (!fs.existsSync(tokensDir)) {
-        fs.mkdirSync(tokensDir);
+const themeTokenSets = tokenConfig.$themes.find((c) => c.name === 'Dark').selectedTokenSets
+
+const theme = {}
+
+Object.keys(themeTokenSets).forEach(
+  (tokenset) => {
+    for (let [key, value] of Object.entries(tokenConfig[tokenset])) {
+      theme[key] = value;
     }
+  }
+);
 
-    // Iterate over the parent properties
-    for (const parent in data) {
-        if (data.hasOwnProperty(parent)) {
-            const childData = data[parent];
-            const childFileName = `${parent}.json`;
-            const childPath = path.join(tokensDir, childFileName);
+const themeJson = JSON.stringify(theme, null, 2)
+console.log(themeJson)
+const systemTokens = 'theme.json';
 
-            // Create the parent directory if it doesn't exist
-            const parentDir = path.dirname(childPath);
-            if (!fs.existsSync(parentDir)) {
-                fs.mkdirSync(parentDir, { recursive: true });
-            }
-
-            // Convert the child data to JSON string
-            const childJsonData = JSON.stringify(childData, null, 2);
-
-            // Write the child JSON file
-            fs.writeFileSync(childPath, childJsonData);
-
-            console.log(`Created ${childFileName}`);
-        }
-    }
-}
-
-// Usage
-const filePath = 'tokens.json';
-splitJSONFile(filePath);
+fs.writeFileSync(systemTokens, themeJson);
