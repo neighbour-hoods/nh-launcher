bf4d3c7f135d8b7e64bc53ca936a62f1a0d8850d
Wesley Finck <wesleyfinck@gmail.com>
2023-09-26 16:41:27 -0700

    check input parameters of resource view callback parameter with passing tests

--- ui/app/src/elements/components/__tests__/resource-view.test.ts
+++ ui/app/src/elements/components/__tests__/resource-view.test.ts
@@ -1,15 +1,19 @@
 import '@webcomponents/scoped-custom-element-registry';
-import { describe, test } from 'vitest';
+import { describe, test, expect as viExpect } from 'vitest';
 import { fixture, html as testHtml, expect } from '@open-wc/testing';
 import { html, TemplateResult } from 'lit';
 import { ResourceView } from '@neighbourhoods/nh-launcher-applet';
 import { vi } from 'vitest';
 import { AppAgentCallZomeRequest } from '@holochain/client';
 
+const ROLE_NAME = 'todo';
+const ZOME_NAME = 'todos';
+const FUNCTION_NAME = 'get_todo';
+
 class TodoView extends ResourceView {
-  roleName = 'todo';
-  zomeName = 'todos';
-  functionName = 'get_todo';
+  roleName = ROLE_NAME;
+  zomeName = ZOME_NAME;
+  functionName = FUNCTION_NAME;
 
   renderPending() {
     return html` <div class="loading-state">Loading...</div> `;
@@ -74,6 +78,27 @@ describe('ResourceView', () => {
       const children = harness.querySelectorAll('todo-view');
       expect(children.length).to.equal(1);
       expect(children[0].shadowRoot?.querySelector('div')?.className).to.equal('error-state');
+    });
+    test(`It calls the parameterized callback with the correct function parameters.`, async () => {
+      const mockResourceResolver = vi.fn(
+        (_request: AppAgentCallZomeRequest) => new Promise(() => {}),
+      );
+      const resourceEh = Uint8Array.from([1, 2, 3, 4]);
+      const _harness = await fixture(testHtml`
+        <div>
+            <todo-view
+              .resourceEh=${resourceEh}  
+              .resourceResolver=${mockResourceResolver}
+            ></todo-view>
+        </div>`);
+
+      // Check if the `mockResourceResolver` has been called with the expected arguments
+      viExpect(mockResourceResolver).toHaveBeenCalledWith({
+        role_name: ROLE_NAME,
+        zome_name: ZOME_NAME,
+        fn_name: FUNCTION_NAME,
+        payload: resourceEh,
+      });
     });
   });
 });
