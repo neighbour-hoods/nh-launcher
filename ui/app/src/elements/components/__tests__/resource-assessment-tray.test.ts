996f78dd2f1a1c1736e2972c22e1013cea6529d1
Wesley Finck <wesleyfinck@gmail.com>
2023-10-06 15:43:12 -0700

    passing test for mocking creating an assessment with the create assessment widget

--- ui/app/src/elements/components/__tests__/resource-assessment-tray.test.ts
+++ ui/app/src/elements/components/__tests__/resource-assessment-tray.test.ts
@@ -1,7 +1,7 @@
 import '@webcomponents/scoped-custom-element-registry';
 
 import { describe, test } from 'vitest';
-import { fixture, html as testHtml, expect } from '@open-wc/testing';
+import { fixture, html as testHtml, expect, oneEvent } from '@open-wc/testing';
 import { html } from 'lit';
 import { AssessmentWidgetConfig, CreateAssessmentWidget, DisplayAssessmentWidget, ResourceAssessmentTray } from '@neighbourhoods/nh-launcher-applet';
 import { EntryHashB64, encodeHashToBase64 } from '@holochain/client';
@@ -9,7 +9,7 @@ import { Assessment, RangeValueFloat } from '@neighbourhoods/client';
 
 class ImportanceAssessmentWidget extends CreateAssessmentWidget {
   render() {
-    return html`<div @click=${this.dispatchCreateAssessment({
+    return html`<div @click=${() => this.dispatchCreateAssessment({
         value: { Float: 1 },
         dimension_eh: this.dimensionEh,
         resource_eh: this.resourceEh,
@@ -40,6 +40,7 @@ const outputAssessment: Assessment = {
 };
 
 customElements.define('resource-assessment-tray', ResourceAssessmentTray);
+customElements.define('importance-assessment-widget', ImportanceAssessmentWidget);
 
 describe('ResourceAssessmentTray', () => {
   describe('Given a ResourceAssessmentTray that takes in one pair of input/output dimension widgets', () => {
@@ -64,24 +65,44 @@ describe('ResourceAssessmentTray', () => {
         }
       },
     });
+    const trayHtml = testHtml`
+    <div>
+      <div class="harness-class"></div>
+      <resource-assessment-tray
+        .assessmentWidgetTrayConfig=${assessmentWidgetTrayConfig}
+        .resourceEh=${resourceEh}
+        .resourceDefEh=${resourceDefEh}
+        .outputAssessments=${outputAssessments}
+      ></resource-assessment-tray>
+    </div>`
 
     test(`It renders the two widgets that get passed in as parameters.`, async () => {
-      const trayHtml = testHtml`
-      <div>
-        <div class="harness-class"></div>
-        <resource-assessment-tray
-          .assessmentWidgetTrayConfig=${assessmentWidgetTrayConfig}
-          .resourceEh=${resourceEh}
-          .resourceDefEh=${resourceDefEh}
-          .outputAssessments=${outputAssessments}
-        ></resource-assessment-tray>
-      </div>`
       const harness = await fixture(trayHtml);
       console.log("harness class length", harness.getElementsByClassName("harness-class").length)
 
       const children = harness.querySelectorAll('resource-assessment-tray');
       expect(children[0].shadowRoot?.querySelectorAll('importance-assessment-widget').length).to.equal(1);
       expect(children[0].shadowRoot?.querySelectorAll('total-importance-widget').length).to.equal(1);
+    });
+
+    test(`It receives a create-assessment event from the CreateAssessmentWidget with the correct payload.`, async () => {
+      const harness = await fixture(trayHtml);
+
+      const resourceAssessmentTray = harness.querySelectorAll('resource-assessment-tray')[0];
+      const importanceAssessmentWidget = resourceAssessmentTray.shadowRoot?.querySelectorAll('importance-assessment-widget')[0]!;
+      const createAssessmentDiv = importanceAssessmentWidget.shadowRoot?.querySelectorAll('div')[0]!;
+
+      const listener = oneEvent(resourceAssessmentTray, 'create-assessment');
+      createAssessmentDiv.click();
+      const event = await listener;
+      const expectedInput = {
+        value: { Float: 1 },
+        dimension_eh: inputDimensionEh,
+        resource_eh: resourceEh,
+        resource_def_eh: resourceDefEh,
+        maybe_input_dataset: null,
+      }
+      expect(event.detail).to.deep.equal(expectedInput);
     });
   });
 });
