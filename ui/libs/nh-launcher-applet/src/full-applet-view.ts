27596e669db1ed346fd4d6583de9f5b3929a9860
Wesley Finck <wesleyfinck@gmail.com>
2023-10-19 11:26:11 -0700

    WIP: full applet view class declaration

--- ui/libs/nh-launcher-applet/src/full-applet-view.ts
+++ ui/libs/nh-launcher-applet/src/full-applet-view.ts
@@ -1,7 +1,13 @@
-import { LitElement, TemplateResult } from "lit"
+import { CSSResult, LitElement } from "lit"
 import { property } from "lit/decorators.js";
 import { AppAgentClient } from "@holochain/client"
 import { AppletInfo, NeighbourhoodServices } from ".";
+
+export interface IFullAppletView {
+  appAgentWebsocket: AppAgentClient
+  neighbourhoodServices: NeighbourhoodServices
+  appletInfo: AppletInfo[]
+}
 
 export abstract class FullAppletView extends LitElement {
   @property()
@@ -13,6 +19,28 @@ export abstract class FullAppletView extends LitElement {
   @property()
   appletInfo!: AppletInfo[]
 
-  abstract render(): TemplateResult;
+  registry?: CustomElementRegistry
+
+  override createRenderRoot() {
+    this.registry = new CustomElementRegistry()
+
+    //@ts-ignore
+    const renderRoot = (this.renderOptions.creationScope = this.attachShadow({
+      mode: 'open',
+      customElements: this.registry,
+    }));
+
+    return renderRoot;
+  }
+
+  connectedCallback(): void {
+    super.connectedCallback();
+    const scopedElements = Object.entries(this.scopedElements());
+    scopedElements.forEach(([name, element]) => {
+      this.registry?.define(name, element);
+   });
+  }
+
+  abstract scopedElements(): { [component: string]: new () => HTMLElement }
 }
 
