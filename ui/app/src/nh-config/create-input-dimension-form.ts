78dbdaedf623329f2641120e22bbb03f97c1ca23
Nicholas Stebbings <nickolarse@riseup.net>
2024-01-19 14:20:04 +1300

    implement nh-form in create-dimension form

--- ui/app/src/nh-config/create-input-dimension-form.ts
+++ ui/app/src/nh-config/create-input-dimension-form.ts
@@ -1,12 +1,12 @@
-import { NHBaseForm, NHButton, NHCard, NHTextInput, NHTooltip } from "@neighbourhoods/design-system-components";
+import { NHBaseForm, NHButton, NHCard, NHComponent, NHForm, NHTextInput, NHTooltip } from "@neighbourhoods/design-system-components";
 import { html, css, CSSResult } from "lit";
 import { SlCheckbox, SlInput, SlRadio, SlRadioGroup } from "@scoped-elements/shoelace";
-import { object, string, number, ObjectSchema } from 'yup';
+import { object, string, number, ObjectSchema, boolean } from 'yup';
 import { Dimension, Range, RangeKind, SensemakerStore, RangeKindFloat, RangeKindInteger } from "@neighbourhoods/client";
 import { property, query, state } from "lit/decorators.js";
 import { MAX_RANGE_FLOAT, MAX_RANGE_INT, MIN_RANGE_FLOAT, MIN_RANGE_INT } from ".";
 
-export default class CreateDimension extends NHBaseForm {
+export default class CreateDimension extends NHComponent {
   @property()
   sensemakerStore!: SensemakerStore;
 
@@ -23,18 +23,20 @@ export default class CreateDimension extends NHBaseForm {
     max: undefined as number | undefined,
   };
 
+  private get isIntegerRangeKind() { return this._numberType == "Integer" }
+
   // Helper to generate nested, dynamic schema for the Range
   private _dimensionRangeSchema = () => {
-    const rangeMin = this._numberType == "Integer" ? MIN_RANGE_INT : MIN_RANGE_FLOAT
-    const rangeMax = this._numberType == "Integer" ? MAX_RANGE_INT : MAX_RANGE_FLOAT
+    const rangeMin = this.isIntegerRangeKind ? MIN_RANGE_INT : MIN_RANGE_FLOAT
+    const rangeMax = this.isIntegerRangeKind ? MAX_RANGE_INT : MAX_RANGE_FLOAT
     const numberType = number().typeError('The input must be numeric').required('Enter a number');
     return {
-      min: (this._numberType == "Integer" 
+      min: (this.isIntegerRangeKind 
           ? numberType.integer('Must be an integer') 
           : numberType.test('is-decimal', 'Must be a decimal number', ((value: number) => value.toString().match(/^(\-)?\d+(\.\d+)?$/)) as any)
         )
         .min(rangeMin, "The lower extent of this range cannot be lower than " + rangeMin),
-      max:(this._numberType == "Integer" 
+      max:(this.isIntegerRangeKind 
           ? numberType.integer('Must be an integer') 
           : numberType.test('is-decimal', 'Must be a decimal number', ((value: number) => value.toString().match(/^\d+(\.\d+)?$/)) as any)
         )
@@ -42,20 +44,18 @@ export default class CreateDimension extends NHBaseForm {
         .max(rangeMax, "The higher extent of this range cannot be higher than " + rangeMax),
   }};
 
-  // Full concrete implementation of form schema
-  protected get validationSchema() : ObjectSchema<any> { 
+  schema() : ObjectSchema<any> { 
     return object({
     name: string().min(1, "Must be at least 1 characters").required("Enter a dimension name, e.g. Likes"),
+    number_type: string().required("Pick an option"),
+    global_min: boolean(),
+    global_max: boolean(),
     ...this._dimensionRangeSchema()
   })};
-
+  
   // Extra form state, not in the model
   @property()
-  private _numberType: (keyof RangeKindInteger | keyof RangeKindFloat) = "Integer";
-  @state()
-  private _useGlobalMin: boolean = false;
-  @state()
-  private _useGlobalMax: boolean = false;
+  private _numberType?: (keyof RangeKindInteger | keyof RangeKindFloat);
 
   @property()
   submitBtn!: NHButton;
@@ -64,15 +64,8 @@ export default class CreateDimension extends NHBaseForm {
   @query("nh-text-input[name='max']")
   _maxInput!: NHTextInput;
 
-  // Form submit handler
-  async handleValidSubmit() {
-    this.submitBtn.loading = true;
-    this.submitBtn.requestUpdate("loading");
-
-    return await this.createEntries()
-  }
-  
-  async createEntries() {
+  async createEntries(model: object) {
+    debugger;
     let rangeEh, dimensionEh;    
     let inputRange: Range = {
       name: this._model.name + '_range',
@@ -118,135 +111,118 @@ export default class CreateDimension extends NHBaseForm {
     );
   }
 
-  handleInputChange(e: Event) {
-    super.handleInputChange(e);
-    
-    // Change handler overloads
-    const inputControl = (e.target as any);
-
-    switch (inputControl?.name || inputControl.parentElement.dataset?.name) {
-      case 'min':
-        this._model.min = Number(inputControl.value); 
-        break;
-      case 'max':
-        this._model.max = Number(inputControl.value);
-        break;
-      case 'use-global-min':
-        this._useGlobalMin = !this._useGlobalMin
-        this._model.min = this._numberType == "Integer" ? MIN_RANGE_INT : MIN_RANGE_FLOAT;
-        this.validateForm()
-        this._minInput?.requestUpdate()
-        break;
-      case 'use-global-max':
-        this._useGlobalMax = !this._useGlobalMax
-        this._model.max = this._numberType == "Integer" ? MAX_RANGE_INT : MAX_RANGE_FLOAT
-        this.validateForm()
-        this._maxInput?.requestUpdate()
-        break;
-      case 'number-type':
-        this._numberType = inputControl.value as (keyof RangeKindInteger | keyof RangeKindFloat);
-        //@ts-ignore
-        if(this._useGlobalMin) {
-          this._model.min = this._numberType == "Integer" ? MIN_RANGE_INT : MIN_RANGE_FLOAT;
-        }
-        if(this._useGlobalMax) {
-          this._model.max = this._numberType == "Integer" ? MAX_RANGE_INT : MAX_RANGE_FLOAT;
-        }
-        break;
-    }
+  async resetForm() {
+    // this._numberType = "Integer";
+    // this._useGlobalMin = false;
+    // this._useGlobalMax = false;
   }
 
-  async resetForm() {
-    super.reset();
-    
-    this._numberType = "Integer";
-    this._useGlobalMin = false;
-    this._useGlobalMax = false;
-    
-    this.submitBtn.loading = false;
-    await this.submitBtn.updateComplete;
-    await this.updateComplete
+  setRangeBoundsByNumberType(model: any) {
+    console.log('this.isIntegerRangeKind :>> ', this.isIntegerRangeKind);
+    if(model.global_min) {
+      model.min = this.isIntegerRangeKind ? MIN_RANGE_INT : MIN_RANGE_FLOAT;
+    }
+    if(model.global_max) {
+      model.max = this.isIntegerRangeKind ? MAX_RANGE_INT : MAX_RANGE_FLOAT;
+    }
   }
 
   render() {
     return html`
-        <form method="post" action="" autoComplete="off">
-          <nh-tooltip .visible=${this.shouldShowValidationErrorForField('name')} .text=${this.getErrorMessage('name')} .variant=${"danger"}>
-            <nh-text-input
-              .errored=${this.shouldShowValidationErrorForField('name')}
-              .size=${"medium"}
-              slot="hoverable"
-              .label=${"Dimension Name"}
-              .name=${"name"}
-              .placeholder=${"Enter a dimension name"}
-              .required=${true}
-              .value=${this._model.name}
-              @change=${(e: CustomEvent) => this.handleInputChange(e)}
-            ></nh-text-input>
-          </nh-tooltip>
+      <nh-form
+        .config=${(() => ({
+          rows: [1, 1, 2, 2],
+          submitBtnRef: (() => this.submitBtn)(),
+          fields: [
+            [{
+              type: 'text',
+              name: "name",
+              id: "dimension-name",
+              defaultValue: "",
+              size: "medium",
+              required: true,
+              placeholder: 'Enter a dimension name',
+              label: 'Dimension Name',
+            }],
 
-          <div class="field radio">
-            <sl-radio-group @sl-change=${(e: CustomEvent) => this.handleInputChange(e)} label=${"Select a number type"} data-name=${"number-type"} value=${this._numberType}>
-              <sl-radio .checked=${this._numberType == "Integer"} value="Integer">Integer</sl-radio>
-              <sl-radio .checked=${this._numberType == "Float"} value="Float">Float</sl-radio>
-            </sl-radio-group>
-          </div>
-          
-        <div class="row-2">
-          <div class="field">
-            <nh-tooltip class="extend" .visible=${this.shouldShowValidationErrorForField('min')} .text=${this.getErrorMessage('min')} .variant=${"danger"}>
-              <nh-text-input
-                .errored=${this.shouldShowValidationErrorForField('min')}
-                .size=${"small"}
-                slot="hoverable"
-                .label=${"Min"}
-                .required=${true}
-                .placeholder=${"Min"}
-                .name=${"min"}
-                .disabled=${this._useGlobalMin}
-                .value=${this._model.min}
-                @change=${(e: CustomEvent) => this.handleInputChange(e)}>
-              </nh-text--input>
-            </nh-tooltip>
-            <div class="field checkbox">
-              <label for="global-min" name="use-global-min">Lowest possible</label>
-              <sl-checkbox name="use-global-min" .checked=${this._useGlobalMin} @sl-change=${(e: CustomEvent) => this.handleInputChange(e)}></sl-checkbox>
-            </div>
-          </div>
-          <div class="field">
-            <nh-tooltip class="extend" .visible=${this.shouldShowValidationErrorForField('max')} .text=${this.getErrorMessage('max')} .variant=${"danger"}>
-              <nh-text-input
-                .errored=${this.shouldShowValidationErrorForField('max')}
-                .size=${"small"}
-                slot="hoverable"
-                .label=${"Max"}
-                .required=${true}
-                .placeholder=${"Max"}
-                .name=${"max"}
-                .disabled=${this._useGlobalMax}
-                .value=${this._model.max}
-                @change=${(e: CustomEvent) => this.handleInputChange(e)}>
-              </nh-text--input>
-            </nh-tooltip>
-            <div class="field checkbox">
-              <label for="global-max" name="use-global-max">Highest possible</label>
-              <sl-checkbox name="use-global-max" .checked=${this._useGlobalMax} @sl-change=${(e: CustomEvent) => this.handleInputChange(e)}></sl-checkbox>
-            </div>
-          </div>
-        </div>
-      </form>
+            [{
+              type: 'radio-group',
+              options: ['Whole', 'Decimal'],
+              name: "number_type",
+              id: "number-type",
+              defaultValue: "",
+              size: "medium",
+              required: true,
+              direction: 'vertical',
+              label: 'Number type',
+              handleInputChangeOverload: (_e, model, _fields) => {
+                this._numberType = model.number_type == 'Decimal' ? 'Float' : 'Integer' // Need to rely on local state for dynamic schema, and translate from Display number type
+                this.setRangeBoundsByNumberType(model)
+              },
+            }],
+
+            [{
+              type: 'text',
+              name: "min",
+              id: "min",
+              defaultValue: "0",
+              size: "small",
+              required: true,
+              placeholder: 'Min',
+              label: 'Min Value',
+              mutateValue: (val: string) => Number(val)
+            },
+            {
+              type: 'text',
+              name: "max",
+              id: "max",
+              defaultValue: "100",
+              size: "small",
+              required: true,
+              placeholder: 'Max',
+              label: 'Max Value',
+              mutateValue: (val: string) => Number(val)
+            }],
+
+            [{
+              type: 'checkbox',
+              name: "global_min",
+              id: "global-min",
+              defaultValue: false,
+              size: "small",
+              required: false,
+              label: 'Use Lowest',
+              handleInputChangeOverload: (e, model, fields) => {
+                model.min = this.isIntegerRangeKind ? MIN_RANGE_INT : MIN_RANGE_FLOAT;
+                if (fields?.min) fields.min.disabled = e.target.value;
+                this.setRangeBoundsByNumberType(model)
+              }},
+            {
+              type: 'checkbox',
+              name: "global_max",
+              id: "global-max",
+              defaultValue: false,
+              size: "small",
+              required: false,
+              label: 'Use Highest',
+              handleInputChangeOverload: (e, model, fields) => {
+                model.max = this.isIntegerRangeKind ? MAX_RANGE_INT : MAX_RANGE_FLOAT;
+                if (fields?.max) fields.max.disabled = e.target.value;
+                this.setRangeBoundsByNumberType(model)
+              }
+            }],
+          ],
+          submitOverload: (model: object) => this.createEntries(model),
+          resetOverload: this.resetForm,
+          progressiveValidation: false,
+          schema: (() => this.schema())() // Relies on dynamic elements so use and IIFE
+        }))()}></nh-form>
     `;
   }
 
   static elementDefinitions = {
-    "nh-button": NHButton,
+    "nh-form": NHForm,
     "nh-card": NHCard,
-    "sl-input": SlInput,
-    "sl-radio": SlRadio,
-    "sl-radio-group": SlRadioGroup,
-    "sl-checkbox": SlCheckbox,
-    "nh-text-input": NHTextInput,
-    "nh-tooltip": NHTooltip,
   }
 
   static get styles() {
@@ -261,99 +237,7 @@ export default class CreateDimension extends NHBaseForm {
           color: var(--nh-theme-fg-default);
           margin: 0 auto;
         }
-
-        form {
-          padding: 0;
-          margin: calc(1px * var(--nh-spacing-md)) 0 calc(1px * var(--nh-spacing-xl)) 0;
-        }
-
-        .row-2 {
-          display: flex;
-          justify-content:space-between;
-          align-items: center;
-          flex-wrap: wrap;
-        }
-        .row-2:last-child {
-          padding-bottom: calc(1px * var(--nh-spacing-md));
-        }
-
-        .field {
-          display: flex;
-          margin-top: calc(1px * var(--nh-spacing-sm));
-          padding-top: calc(1px * var(--nh-spacing-md));
-        }
-
-        .row-2 .field {
-          flex-basis: 50%;
-          margin: 0;
-          flex-direction: column;
-        }
-        .row-2 .field:first-child {
-          align-items: flex-start;
-        }
-        .row-2 .field:last-child {
-          align-items: flex-end;
-        }
-
-        /* Radio */
-
-        .field.radio {
-          justify-content: center;  
-          padding-top: calc(1px * var(--nh-spacing-md));
-          margin-top: calc(1px * var(--nh-spacing-lg));
-        }
-
-        sl-radio-group, sl-radio-group::part(base) {
-          width: 100%;
-        }
-          
-        sl-radio-group::part(base) {
-          display: flex;
-          justify-content: space-around;
-          gap: calc(1px * var(--nh-spacing-md));
-        }
-
-        sl-radio:hover::part(control) {
-          background-color: var(--nh-theme-bg-detail); 
-        }
-
-        sl-radio::part(label) {
-          color: var(--nh-theme-fg-default);
-        }
-
-        sl-radio::part(control) {
-          color: var(--nh-theme-accent-default);
-          border-color: var(--nh-theme-accent-default);
-          background-color: transparent;
-        }
-
-        sl-radio {
-          margin-bottom: 0 !important;
-        }
-
-        /* Checkbox */
-
-        sl-checkbox::part(base) {
-          position: relative;
-          right: -9px;
-          bottom: 1px
-        }
-
-        sl-checkbox::part(control) {
-          color: var(--nh-theme-accent-default);
-          background-color: var(--nh-theme-fg-default);
-          border-color: var(--nh-theme-accent-default);
-          border-radius: 3px;
-        }
-
-        .field.checkbox {
-          justify-content: end;
-          display: flex;
-          width: 8rem;
-          font-size: 90%;
-          flex-direction: initial;
-        }
-    `
+      `
     ]
   }
 }
