92b2a723150f3c3acda59f4a411653d54226e7e4
Nicholas Stebbings <nickolarse@riseup.net>
2024-01-26 17:31:13 +1300

    reorganise into folders for components

---
+++ ui/app/src/nh-config/lists/dashboard-table.ts
@@ -0,0 +1,275 @@
+import { html, css, TemplateResult } from 'lit';
+import { property, customElement, state } from 'lit/decorators.js';
+import { ref } from "lit/directives/ref.js";
+
+import {
+  FieldDefinitions,
+  FieldDefinition,
+  TableStore,
+  Table,
+} from '@adaburrows/table-web-component';
+
+import { SlAlert, SlSkeleton } from '@scoped-elements/shoelace';
+import { NHComponentShoelace } from '@neighbourhoods/design-system-components';
+
+// import { WithProfile } from './profile/with-profile';
+import { consume } from '@lit/context';
+import { AgentPubKeyB64, DnaHash, encodeHashToBase64 } from '@holochain/client';
+import { weGroupContext } from '../../context';
+import { AssessmentTableRecord, AssessmentTableType, assessmentTableId } from '../types';
+
+
+@customElement('dashboard-table')
+export class DashboardTable extends NHComponentShoelace {
+  @consume({ context: matrixContext, subscribe: true })
+  @property({ attribute: false })
+  _matrixStore!: MatrixStore;
+
+  @consume({ context: weGroupContext, subscribe: true })
+  @property({ attribute: false })
+  weGroupId!: DnaHash;
+
+  @property({ type: Array })
+  assessments: AssessmentTableRecord[] = [];
+
+  @property({ type: Object })
+  tableStore!: TableStore<AssessmentTableRecord>;
+
+  @property()
+  resourceName!: string;
+  @state()
+  columns: number = 0;
+  @state()
+  loading: boolean = true;
+  @property()
+  contextFieldDefs!: { [x: string]: FieldDefinition<AssessmentTableRecord> };
+  @property()
+  tableType!: AssessmentTableType;
+
+  updateTable() {
+    this.tableStore.fieldDefs = this.generateFieldDefs(this.resourceName, this.contextFieldDefs);
+
+    // Check if we have the necessary data to create the table
+    if (!this.assessments || !this.tableStore.fieldDefs) {
+      console.warn('No data or field definitions to create table.');
+      return;
+    }
+    
+    // The following line removes records in the table that have no assessment value for the context field definitions generate by generateFieldDefs
+    this.contextFieldDefs && Object.entries(this.contextFieldDefs).length  && 
+      (this.tableStore.records = this.assessments.filter(assessment => Object.keys(this.contextFieldDefs).some(contextField => assessment[contextField] !== "")) as AssessmentTableRecord[] )
+      
+    if(typeof this.contextFieldDefs == 'object') this.columns = Object.values(this.contextFieldDefs).length + 2
+  }
+  
+  async connectedCallback() {
+    super.connectedCallback();
+
+    let fieldDefs = this.generateFieldDefs(this.resourceName, this.contextFieldDefs);
+    this.tableStore = new TableStore({
+      tableId: assessmentTableId,
+      fieldDefs,
+      colGroups: [{ span: 2, class: 'fixedcols' }],
+      showHeader: true,
+      records: [] as AssessmentTableRecord[]
+    });
+  }
+
+  updated(changedProps) {
+    if (changedProps.has('assessments') || changedProps.has('contextFieldDefs') || changedProps.has('resourceName')) {
+      this.updateTable();
+      this.loading = false;
+    }
+  }
+
+  refMemo = {}
+
+  getRef(resource: any) {
+    if (typeof resource.eh[1] !== 'function') return
+
+    const hashKey = encodeHashToBase64(resource.eh[0])  + resource.value[1].timestamp;
+    // if (this.refMemo[hashKey]) return this.refMemo[hashKey]
+    //TODO rework this to fix bug when implemented.
+    const callback = function(e) { if(!e) return; return resource.eh[1](e, resource.eh[0]) }.bind(this);
+    const myref = ref(callback); 
+    this.refMemo[hashKey] = { callback, ref: myref }
+    return myref
+  }
+
+  generateFieldDefs(
+    resourceName: string,
+    contextFieldDefs: { [x: string]: FieldDefinition<AssessmentTableRecord> },
+  ): FieldDefinitions<AssessmentTableRecord> {
+    const fixedFieldDefs = {
+      resource: new FieldDefinition<AssessmentTableRecord>({
+        heading: generateHeaderHTML('Resource', resourceName),
+        decorator: (resource: any) => {
+          return html`<div
+          style="width: 100%; display: grid;place-content: start center; height: 100%; justify-items: center;"
+          ${this.getRef(resource)}
+        >
+        </div>`},
+      }),
+      neighbour: new FieldDefinition<AssessmentTableRecord>({
+        heading: generateHeaderHTML('Neighbour', 'Member'),
+        decorator: (agentPublicKeyB64: AgentPubKeyB64) => {
+          return html` <div
+          style="width: 100%; display: flex; flex-direction: column; align-items: start; height: 100%; justify-items: center;"
+          >
+            ${generateHashHTML(agentPublicKeyB64)}
+        </div>`}},
+      ),
+    };
+    return {
+      ...fixedFieldDefs,
+      ...contextFieldDefs,
+    };
+  }
+
+  render(): TemplateResult {
+    return !this.loading && this.contextFieldDefs
+      ? html`<wc-table .tableStore=${this.tableStore}></wc-table>`
+      : html`<div class="skeleton-main-container" data-columns=${this.columns}>
+      ${Array.from(Array(this.columns)).map(
+
+        () => html`<sl-skeleton effect="pulse" class="skeleton-part-header"></sl-skeleton>`,
+      )}
+      ${Array.from(Array(this.columns * 5)).map(
+
+        () => html`<sl-skeleton effect="pulse" class="skeleton-part"></sl-skeleton>`,
+      )}
+    </div>`;
+  }
+
+  static elementDefinitions = {
+    'wc-table': Table,
+    'sl-alert': SlAlert,
+    'we-group-context': WeGroupContext,
+    'sl-skeleton': SlSkeleton,
+    'with-profile': WithProfile,
+  };
+
+  static styles = css`
+    :host {
+      /** Global Table **/
+      color: var(--nh-theme-fg-default);
+      --table-assessmentsForResource-height: 100%;
+      --table-assessmentsForResource-overflow-x: auto;
+      --table-assessmentsForResource-overflow-y: auto;
+      --table-assessmentsForResource-max-height: calc(
+        100vh - 101px - calc(1px * var(--nh-spacing-lg))
+      );
+
+      --table-assessmentsForResource-border-spacing: calc(1px * var(--nh-spacing-sm));
+      --cell-radius: calc(1px * var(--nh-radii-base));
+
+      --table-assessmentsForResource-display: block;
+      --table-vertical-align: middle;
+      --border-color: #7d7087;
+      --menuSubTitle: #a89cb0;
+      --column-min-width: calc(1rem * var(--nh-spacing-sm));
+      --column-max-width: calc(1rem * var(--nh-spacing-md));
+
+      /** Header Cells **/
+      --table-assessmentsForResource-heading-background-color: var(--nh-theme-bg-surface);
+      --header-cell-border-width: 1px;
+      --header-title-margin-y: 6px;
+      --table-assessmentsForResource-importance-heading-vertical-align: bottom;
+
+      /* Border color, width */
+      --table-assessmentsForResource-header-first-heading-border-color: var(--border-color);
+      --table-assessmentsForResource-header-last-heading-border-color: var(--border-color);
+      --table-assessmentsForResource-header-heading-border-color: var(--border-color);
+      --table-assessmentsForResource-header-first-heading-border-width: var(
+        --header-cell-border-width
+      );
+      --table-assessmentsForResource-header-last-heading-border-width: var(
+        --header-cell-border-width
+      );
+      --table-assessmentsForResource-header-heading-border-width: var(--header-cell-border-width);
+      /* Border radius */
+      --table-assessmentsForResource-header-first-heading-border-radius: var(--cell-radius);
+      --table-assessmentsForResource-header-last-heading-border-radius: var(--cell-radius);
+      --table-assessmentsForResource-header-heading-border-radius: var(--cell-radius);
+      --table-assessmentsForResource-body-first-cell-border-radius: var(--cell-radius);
+      --table-assessmentsForResource-body-last-cell-border-radius: var(--cell-radius);
+      --table-assessmentsForResource-body-cell-border-radius: var(--cell-radius);
+
+      /** Global Cells **/
+      /* Hashes */
+      --table-assessmentsForResource-body-cell-background-color: var(--nh-theme-bg-surface);
+      --cell-hash-border-radius: calc(1px * var(--nh-radii-sm));
+      --cell-hash-padding: calc(1px * var(--nh-spacing-sm));
+      --cell-hash-font-size: calc(1px * var(--nh-font-size-sm));
+
+      /* Values */
+      --cell-value-font-size: calc(1px * var(--nh-font-size-md));
+
+      /** First Two Columns **/
+      --table-assessmentsForResource-resource-width: var(--column-max-width);
+      --table-assessmentsForResource-neighbour-width: var(--column-max-width);
+      --table-assessmentsForResource-resource-vertical-align: middle;
+      --table-assessmentsForResource-neighbour-vertical-align: middle;
+
+      --table-assessmentsForResource-row-even-background-color: var(---nh-theme-bg-surface);
+      --table-assessmentsForResource-row-odd-background-color: var(---nh-theme-bg-surface);
+      --table-assessmentsForResource-resource-row-even-background-color: var(
+        --nh-colors-eggplant-800
+      ) !important;
+      --table-assessmentsForResource-resource-heading-background-color: var(
+        --nh-colors-eggplant-800
+      ) !important;
+      --table-assessmentsForResource-neighbour-heading-background-color: var(
+        --nh-colors-eggplant-800
+      ) !important;
+      --table-assessmentsForResource-row-even-background-color: var(--nh-theme-bg-surface);
+      --table-assessmentsForResource-row-odd-background-color: var(--nh-theme-bg-surface);
+    }
+    .alert-wrapper {
+      height: 100%;
+      display: grid;
+      place-content: center;
+      align-content: start;
+      padding: 4rem calc(1px * var(--nh-spacing-lg));
+    }
+    .alert::part(base) {
+      height: 8rem;
+      width: 100%;
+    }
+
+    .skeleton-main-container {
+      display: grid;
+      grid-template-columns: 204px 204px repeat(2, 140px);
+      gap: calc(1px * var(--nh-spacing-sm));
+      margin: calc(1px * var(--nh-spacing-sm));
+      grid-template-rows: 86px repeat(8, 4rem);
+    }
+    .skeleton-main-container[data-columns="3"] {
+      grid-template-columns: 204px 204px repeat(1, 140px);
+    }
+    .skeleton-main-container[data-columns="4"] {
+      grid-template-columns: 204px 204px repeat(2, 140px);
+    }
+    .skeleton-main-container[data-columns="5"] {
+      grid-template-columns: 204px 204px repeat(3, 140px);
+    }
+    .skeleton-main-container[data-columns="6"] {
+      grid-template-columns: 204px 204px repeat(4, 140px);
+    }
+    .skeleton-part-header {
+      --color: rgb(37, 31, 40);
+      --sheen-color: rgb(37, 31, 40);
+    }
+    .skeleton-part {
+      --color: var(--nh-theme-bg-surface);
+      --sheen-color: var(--nh-theme-bg-surface);
+    }
+    .skeleton-part-header::part(indicator), .skeleton-part::part(indicator) {
+      border-radius: calc(1px * var(--nh-radii-base));
+      opacity: 1;
+    }
+    .skeleton-part-header::part(indicator) {
+      border: 1px solid rgb(125, 112, 135);
+    }
+  `;
+}
---
+++ ui/app/src/nh-config/lists/dimension-list.ts
@@ -0,0 +1,230 @@
+import { html, css, PropertyValueMap } from "lit";
+import { property, state } from "lit/decorators.js";
+
+import { EntryHash, encodeHashToBase64 } from "@holochain/client";
+import { Dimension,  Method,  Range, RangeKind, SensemakerStore } from "@neighbourhoods/client";
+
+import { NHButton, NHCard, NHComponent } from "@neighbourhoods/design-system-components";
+import { capitalize } from "../../elements/components/helpers/functions";
+import { FieldDefinition, FieldDefinitions, Table, TableStore } from "@adaburrows/table-web-component";
+import { EntryRecord } from "@holochain-open-dev/utils";
+
+type InputDimensionTableRecord = {
+  ['dimension-name']: string,
+  ['range-type']: string,
+  ['range-min']: number,
+  ['range-max']: number,
+}
+
+type OutputDimensionTableRecord = InputDimensionTableRecord & {
+  ['input-dimension-name'] : string,
+  ['method-operation'] : string,
+}
+
+type DimensionTableRecord = InputDimensionTableRecord | OutputDimensionTableRecord;
+
+export default class DimensionList extends NHComponent {  
+  @property()
+  sensemakerStore!: SensemakerStore;
+
+  @property()
+  dimensionType: "input" | "output" = "input";
+
+  @state()
+  tableStore!: TableStore<DimensionTableRecord>;
+
+  @state()
+  private _dimensionEntries!: Array<Dimension & { dimension_eh: EntryHash }>;
+
+  @state()
+  private _rangeEntries!: Array<Range & { range_eh: EntryHash }>;
+  
+  @state()
+  private _methodEntries!: Array<Method>;
+
+  async fetchDimensionEntries() {
+    try {
+      const entryRecords = await this.sensemakerStore.getDimensions();
+      this._dimensionEntries = entryRecords.map(entryRecord => {
+        return {
+          ...entryRecord.entry,
+          dimension_eh: entryRecord.entryHash
+        }
+      })
+    } catch (error) {
+      console.log('Error fetching dimension details: ', error);
+    }
+  }
+
+  async fetchRangeEntriesFromHashes(rangeEhs: EntryHash[]) {
+    let response;
+    try {
+      response = await Promise.all(rangeEhs.map(eH => this.sensemakerStore.getRange(eH)))
+    } catch (error) {
+      console.log('Error fetching range details: ', error);
+    }
+    this._rangeEntries = response.map((entryRecord) => ({...entryRecord.entry, range_eh: entryRecord.entryHash})) as Array<Range & { range_eh: EntryHash }>;
+  }
+
+  async firstUpdated(_changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>) {
+    try {
+      await this.fetchDimensionEntries();
+      if(!this._dimensionEntries) return
+      await this.fetchRangeEntriesFromHashes(this._dimensionEntries.map((dimension: Dimension) => dimension.range_eh));
+
+      this._methodEntries = (await (this.sensemakerStore.getMethods()) as Array<EntryRecord<Method>>).map(eR => eR.entry);
+    } catch (error) {
+      console.error('Could not fetch: ', error)
+    }
+  }
+
+  protected updated(changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>): void {
+    if(changedProperties.has('_dimensionEntries') || changedProperties.has('_rangeEntries') || changedProperties.has('_methodEntries')) {
+      if(typeof this._rangeEntries == 'undefined') return;
+      
+      try {
+        const tableRecords = this._dimensionEntries.filter((dimension: Dimension) => this.dimensionType == 'input' ? !dimension.computed : dimension.computed)
+          .reverse()
+          .map((dimension: Dimension & { dimension_eh: EntryHash; }) => {
+            const range = this._rangeEntries
+              .find((range: Range & { range_eh: EntryHash; }) =>
+                encodeHashToBase64(range.range_eh) === encodeHashToBase64(dimension.range_eh));
+                if(!range) {
+                  return {
+                    ['dimension-name']: dimension.name,
+                    ['range-type']: 'N/A',
+                    ['range-min']: 0,
+                    ['range-max']: 0,
+                  }
+                }
+            const method = (this._methodEntries || [])
+              .find((method: Method) =>
+                encodeHashToBase64(method.output_dimension_eh) === encodeHashToBase64(dimension.dimension_eh));
+            const inputDimension = !!method 
+              ? this._dimensionEntries
+                .find((dimension: Dimension & { dimension_eh: EntryHash; }) =>
+                  encodeHashToBase64(method.input_dimension_ehs[0]) === encodeHashToBase64(dimension.dimension_eh))
+              : {};
+
+            const [[rangeType, rangeValues]] : any = Object.entries(range?.kind as RangeKind);
+            
+            return {
+              ['dimension-name']: dimension.name,
+              ['range-type']: rangeType,
+              ['range-min']: rangeValues?.min,
+              ['range-max']: rangeValues?.max,
+              // For output dimensions
+              ['input-dimension-name']: (inputDimension as any)?.name || '',
+              ['method-operation']: typeof method?.program == 'object' ? Object.keys(method.program)[0] : '',
+            }
+          });
+        this.tableStore.records = tableRecords;
+        this.requestUpdate()
+      } catch (error) {
+        console.log('Error mapping dimensions and ranges to table values: ', error)
+      }
+    }
+  }
+
+  async connectedCallback() {
+    super.connectedCallback();
+    
+    const fieldDefs: FieldDefinitions<DimensionTableRecord> = this.dimensionType == "input"
+      ? {
+        'dimension-name': new FieldDefinition<DimensionTableRecord>({heading: 'Name'}),
+        'range-type': new FieldDefinition<DimensionTableRecord>({heading: 'Type'}),
+        'range-min': new FieldDefinition<DimensionTableRecord>({heading: 'Min'}),
+        'range-max': new FieldDefinition<DimensionTableRecord>({heading: 'Max'}) }
+      : {
+        'dimension-name': new FieldDefinition<DimensionTableRecord>({heading: 'Name'}),
+        'input-dimension-name': new FieldDefinition<DimensionTableRecord>({heading: 'Input Dimension'}),
+        'range-type': new FieldDefinition<DimensionTableRecord>({heading: 'Operation'}),
+        'method-operation': new FieldDefinition<DimensionTableRecord>({heading: 'Type'}),
+        'range-min': new FieldDefinition<DimensionTableRecord>({heading: 'Min'}),
+        'range-max': new FieldDefinition<DimensionTableRecord>({heading: 'Max'}) }
+
+    //@ts-ignore
+    this.tableStore = new TableStore({
+      tableId: 'dimensions',
+      fieldDefs,
+      showHeader: true,
+      records: []
+    });
+  }
+  
+  render() {
+    return html`
+      <div class="content">
+        <div class="title-bar">
+          <h1>${capitalize(this.dimensionType)} Dimensions</h1>
+          <slot class="action" name="action-button"></slot>
+        </div>  
+        ${this.tableStore.records && this.tableStore.records.length > 0
+          ? html`<wc-table .tableStore=${this.tableStore}></wc-table>`
+          : 'No dimensions present'
+        }
+      </div>
+    `;
+  }
+
+
+  static elementDefinitions = {
+    "nh-button": NHButton,
+    "nh-card": NHCard,
+    'wc-table': Table,
+  }
+
+  static get styles() {
+    return css`
+      .title-bar {
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+      }
+
+      h1 {
+        display: flex;
+        margin-right: calc(1px * var(--nh-spacing-xl));
+      }
+
+      .action {
+        display: flex;
+        flex: 1;
+      }
+
+      :host {
+        display: flex;
+        flex: 1;
+        justify-content: center;
+        align-items: center;
+
+        /** Global Table **/
+        --table-dimensions-background-color: var(--nh-theme-bg-surface); 
+        --table-dimensions-row-odd-background-color: var(--nh-theme-bg-element); 
+        --table-dimensions-row-even-background-color: var(--nh-theme-bg-element); 
+
+        --table-dimensions-cell-height: 58px;
+
+        --table-dimensions-element-padding: 4px;
+        --table-dimensions-border-spacing: calc(1px * var(--nh-spacing-sm));
+
+        --table-dimensions-height: 100%;
+        --table-dimensions-overflow-x: auto;
+        --table-dimensions-overflow-y: auto;
+
+        /* Border radius */
+        --cell-radius: 5px;
+        --table-dimensions-header-first-heading-border-radius: var(--cell-radius);
+        --table-dimensions-header-last-heading-border-radius: var(--cell-radius);
+        --table-dimensions-header-heading-border-radius: var(--cell-radius);
+        --table-dimensions-body-first-cell-border-radius: var(--cell-radius);
+        --table-dimensions-body-last-cell-border-radius: var(--cell-radius);
+        --table-dimensions-body-cell-border-radius: var(--cell-radius);
+      }
+  
+      .content{
+        width: 100%;
+      }
+    `;
+  }
+}
