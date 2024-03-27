3f0f84c36956edfc6c694d710358c9783146aa87
Nicholas Stebbings <nickolarse@riseup.net>
2024-03-27 14:24:16 +1300

    add config dimension list component (separate to dimension list)

--- ui/app/src/nh-config/index.ts
+++ ui/app/src/nh-config/index.ts
@@ -5,6 +5,7 @@ import NHDashboardOverview from './pages/nh-dashboard-overview'
 import CreateDimension from './forms/create-input-dimension-form'
 import CreateOutputDimensionMethod from './forms/create-output-dimension-form'
 import DimensionList from './lists/dimension-list'
+import ConfigDimensionList from './lists/config-dimension-list'
 import NHContextSelector from './nh-context-selector'
 
 export const MIN_RANGE_INT = 0;
@@ -14,4 +15,4 @@ export const MAX_RANGE_FLOAT = Number.MAX_SAFE_INTEGER;
 
 export const DEFAULT_RANGE_MIN = 0;
 
-export { NHGlobalConfig, NHContextSelector, NHDashboardOverview, NHDimensionConfig, NHAssessmentWidgetConfig, CreateOutputDimensionMethod, CreateDimension, DimensionList }
+export { NHGlobalConfig, NHContextSelector, NHDashboardOverview, NHDimensionConfig, NHAssessmentWidgetConfig, CreateOutputDimensionMethod, CreateDimension, DimensionList, ConfigDimensionList }
