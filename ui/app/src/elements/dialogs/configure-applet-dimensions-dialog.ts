ba8a9f21b3bcc665a70032d5023a6dddfec2f2bb
Nicholas Stebbings <nickolarse@riseup.net>
2024-03-27 14:23:55 +1300

    lay out dimension lists in dialog

--- ui/app/src/elements/dialogs/configure-applet-dimensions-dialog.ts
+++ ui/app/src/elements/dialogs/configure-applet-dimensions-dialog.ts
@@ -2,36 +2,43 @@ import { html, css, CSSResult} from "lit";
 import { property, query } from "lit/decorators.js";
 
 import { NHComponentShoelace, NHDialog, NHForm } from "@neighbourhoods/design-system-components";
-import { DimensionList } from "../../nh-config";
-import { Dimension } from "@neighbourhoods/client";
+import { ConfigDimensionList } from "../../nh-config";
+import { AppletConfigInput } from "@neighbourhoods/client";
 
 export class ConfigureAppletDimensions extends NHComponentShoelace {
   @query('nh-dialog') dialog!: NHDialog;
 
   @property() handleSubmit!: Function;
-  @property() existingDimensions!: Array<Dimension>;
+  @property() config!: AppletConfigInput;
 
   render() {
     return html`
       <nh-dialog
         id="dialog"
-        dialogType="input-form"
-        title="Configure Applet Dimensions"
+        .dialogType=${"widget-config"}
+        .size=${"large"}
+        .title="Configure Applet Dimensions"
         .handleOk=${this.handleSubmit}
       >
         <div slot="inner-content" class="dialog-container">
-          ${ this.existingDimensions && this.existingDimensions.length > 0 
-              ? html`<h2>
-                      ${'Configure Applet Dimensions'}
-                    </h2>
-                    <dimension-list
-                      id="applets-dimension-list"
-                      .dimensionType=${'input'}
-                      .existingDimensionEntries=${this.existingDimensions}
-                    >
-                    </dimension-list>
+          ${ this.config 
+              ? html`
+                      <config-dimension-list
+                        id="applet-input-dimension-list"
+                        .dimensionType=${'input'}
+                        .configDimensions=${(this.config as AppletConfigInput)!.dimensions!.filter(dimension => !dimension.computed)}
+                        .configMethods=${(this.config as AppletConfigInput)!.methods}
+                      >
+                      </config-dimension-list>
+                      <config-dimension-list
+                        id="applet-output-dimension-list"
+                        .dimensionType=${'output'}
+                        .configDimensions=${(this.config as AppletConfigInput)!.dimensions!.filter(dimension => dimension.computed)}
+                        .configMethods=${(this.config as AppletConfigInput)!.methods}
+                      >
+                      </config-dimension-list>
                   `
-              : html`No dimensions!` // Need to add loading spinner instead?
+              : html`No config!` // Need to add loading spinner instead?
             } 
         </div>
       </nh-dialog>
@@ -41,12 +48,16 @@ export class ConfigureAppletDimensions extends NHComponentShoelace {
   static elementDefinitions = {
     'nh-dialog' : NHDialog,
     'nh-form' : NHForm,
-    'dimension-list' : DimensionList
+    'config-dimension-list' : ConfigDimensionList
   }
 
   static styles : CSSResult[] = [
       super.styles as CSSResult,
       css`
+        config-dimension-list {
+          width: 100%;
+        }
+
         .dialog-container {
           display: flex;
           width: 100%;
