51c3a0f7fb842a4f1ac3d984defb6ae7706da8a7
Nicholas Stebbings <nickolarse@riseup.net>
2024-04-22 16:17:09 +1200

    remove old widget code from launcher

--- ui/app/src/widgets/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./average-star-dimension-display"
-export * from "./star-dimension-assessment"
-export * from "./thumbs-up-dimension-assessment"
-export * from "./total-thumbsup-dimension-display"
