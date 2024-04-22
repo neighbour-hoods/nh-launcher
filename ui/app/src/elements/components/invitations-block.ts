b318c0b2cf7402db69bbc555f9e73d048db035d8
Nicholas Stebbings <nickolarse@riseup.net>
2024-04-22 16:14:13 +1200

    remove sl-input and mwc-snackbars

--- ui/app/src/elements/components/invitations-block.ts
+++ ui/app/src/elements/components/invitations-block.ts
@@ -1,16 +1,11 @@
 import { css, CSSResult, html } from "lit";
-import {
-  TextField,
-  Snackbar,
-} from "@scoped-elements/material-web";
 import { consume } from "@lit/context";
 import { property, query, state } from "lit/decorators.js";
 
 import { MatrixStore } from "../../matrix-store";
 import { matrixContext, weGroupContext } from "../../context";
 import { DnaHash, AgentPubKeyB64, decodeHashFromBase64 } from "@holochain/client";
-import { NHButton, NHCard, NHComponentShoelace } from "@neighbourhoods/design-system-components";
-import { SlInput } from "@scoped-elements/shoelace";
+import { NHButton, NHCard, NHComponentShoelace, NHTextInput } from "@neighbourhoods/design-system-components";
 import { b64images } from "@neighbourhoods/design-system-styles";
 
 export class InvitationsBlock extends NHComponentShoelace {
@@ -26,104 +21,101 @@ export class InvitationsBlock extends NHComponentShoelace {
   @state()
   _inviteePubKey: AgentPubKeyB64 | undefined;
 
-  @query("#snackbar-success")
-  _snackbarSuccess!: Snackbar;
-
-  @query("#snackbar-error")
-  _snackbarError!: Snackbar;
-
-  @query("#pubkey-field")
-  _pubkeyField!: TextField;
+  @query("nh-text-input")
+  _pubkeyField!: NHTextInput;
 
   async inviteToJoin(agentPubKey: AgentPubKeyB64) {
-
     this._matrixStore
       .inviteToJoinGroup(this.weGroupId, decodeHashFromBase64(agentPubKey))
       .then((r) => {
-        this._pubkeyField.value = "";
+        this._pubkeyField._input.value = "";
         this._inviteePubKey = undefined;
-        this._snackbarSuccess.show();
+        this.dispatchEvent(
+          new CustomEvent("trigger-alert", {
+            detail: { 
+              title: "Invitation sent!",
+              msg: "Your neighbour should now have an invite in their Neighbourhood Home.",
+              type: "success",
+              closable: true,
+            },
+            bubbles: true,
+            composed: true,
+          })
+        )
       })
       .catch((e) => {
-        this._snackbarError.show();
+        
+      this.dispatchEvent(
+        new CustomEvent("trigger-alert", {
+          detail: { 
+            title: "Error. Public key may be invalid.",
+            msg: "Please check that you have copied it from the correct place!",
+            type: "danger",
+            closable: true,
+          },
+          bubbles: true,
+          composed: true,
+        })
+      );
         console.log(e);
       });
   }
 
   render() {
     return html`
-      <mwc-snackbar
-        id="snackbar-success"
-        timeoutMs="4000"
-        labelText="Invitation sent."
-      ></mwc-snackbar>
-      <mwc-snackbar
-        id="snackbar-error"
-        timeoutMs="4000"
-        labelText="Error. Public key may be invalid."
-      ></mwc-snackbar>
-
       <nh-card .theme=${"dark"} .heading=${"Invite new neighbour"} .textSize=${"sm"}>
         <div class="content">
           <div class="input-pub-key">
-            <div class="field-set">
-              <sl-input
-                help-text="Ask a friend to send you their public key"
+            <form class="invite-form">
+              <nh-text-input
                 id="pubkey-field"
-                name="pubkey-field"
-                type="text"
-                size="small"
-                placeholder="Public Key"
-                required
-                @sl-input=${(e) => (this._inviteePubKey = e.target.value)}
-              ></sl-input>
-            </div>
-            <nh-button .variant=${"primary"} .iconImageB64=${b64images.icons.forwardArrow} @click=${() => this.inviteToJoin(this._inviteePubKey!)} .size=${"md"} .disabled=${!this._inviteePubKey}>Invite</nh-button>
+                .name="pubkey-field"
+                .size=${"small"}
+                .placeholder=${"Public Key"}
+                .required=${true}
+                @change=${(e) => (this._inviteePubKey = e.target.value)}
+              ></nh-text-input>
+              <nh-button type="submit" .variant=${"primary"} .iconImageB64=${b64images.icons.forwardArrow} @click=${() => this.inviteToJoin(this._inviteePubKey!)} .size=${"md"} .disabled=${!this._inviteePubKey}>Invite</nh-button>
+            </form>
           </div>
         </div>
       </nh-card>
     `;
   }
 
-  static get elementDefinitions() {
-    return {
-      "nh-button": NHButton,
-      "sl-input": SlInput,
-      "mwc-snackbar": Snackbar,
-      "nh-card": NHCard,
-    };
+  static elementDefinitions = {
+    "nh-button": NHButton,
+    "nh-text-input": NHTextInput,
+    "nh-card": NHCard,
   }
 
   static get styles() {
     return [
       super.styles as CSSResult,
       css`
-      sl-input::part(input) {
-        --sl-input-spacing-small: calc(1px * var(--nh-spacing-sm));
-      }
-      sl-input::part(base) {
-        height: 3rem !important;
-        margin-bottom: 4px;
-      }
-        .input-pub-key {
-          align-items: flex-start;
-          justify-content: space-between;
-        }
-
         .content {
           max-width: 20rem;
           margin: 0 auto;
-        }
-
-        .content, .field-set {
           flex-direction: column;
         }
 
-        .content, .input-pub-key, .input-pub-key > *, .field-set {
+        .invite-form {
+          display: flex;
+          flex-direction: row;
+          gap: 1rem;
+          justify-content: center;
+          align-items: flex-end;
+        }
+
+        .content, .input-pub-key, .input-pub-key > *, .invite-form {
           display: flex;
         }
 
-        .content, .field-set, .input-pub-key {
+        nh-button {
+          margin-bottom: 4px;
+        }
+
+        .content, .invite-form, .input-pub-key {
           gap: calc(1px * var(--nh-spacing-sm));
         }
     `];
