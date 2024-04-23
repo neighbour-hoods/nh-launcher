e8efa223a73c4baf4d5a51fb9ca505a31d7826cc
Nicholas Stebbings <nickolarse@riseup.net>
2024-04-24 10:28:16 +1200

    get rid of NHComponentSholace as this is not how we will overrde styles

--- ui/libs/design-system-styles/src/index.ts
+++ ui/libs/design-system-styles/src/index.ts
@@ -1,7 +1,3 @@
-import { CSSResult, css, unsafeCSS } from 'lit';
-
-// @ts-ignore
-import Shoelace from './global/css/shoelace-adapter.css';
 // @ts-ignore
 import b64fonts from './global/css/fonts.css';
 // @ts-ignore
@@ -9,8 +5,6 @@ import b64images from './b64images.ts';
 // @ts-ignore
 import Dark from './themes/dark/css/variables.css';
 
-const LitCSS: CSSResult = css`${unsafeCSS(Dark)}`
-
 export {
-  Dark, b64images, b64fonts, LitCSS, Shoelace
+  Dark, b64images, b64fonts
 }
