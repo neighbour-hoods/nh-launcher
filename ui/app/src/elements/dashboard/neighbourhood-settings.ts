a0dbe7abb9bc9c1e00e0950fe2b4a76de48196ec
Nicholas Stebbings <nickolarse@riseup.net>
2024-04-23 15:14:36 +1200

    mwc purge - clean up neighbourhood settings

--- ui/app/src/elements/dashboard/neighbourhood-settings.ts
+++ ui/app/src/elements/dashboard/neighbourhood-settings.ts
@@ -1,7 +1,6 @@
 import { ProfilesStore, profilesStoreContext } from "@holochain-open-dev/profiles";
-import { DnaHash, EntryHash } from "@holochain/client";
+import { DnaHash } from "@holochain/client";
 import { consume } from "@lit/context";
-import {  CircularProgress, LinearProgress, Snackbar } from "@scoped-elements/material-web";
 import { css, html } from "lit";
 import { StoreSubscriber } from "lit-svelte-stores";
 import { property, query, state } from "lit/decorators.js";
@@ -11,12 +10,11 @@ import { AppletInstanceStatusList } from "../components/applet-instance-status-l
 import { UninstalledAppletInstanceList } from "../components/uninstalled-applet-instance-list";
 import { JoinableAppletInstanceList } from "../components/joinable-applet-instance-list";
 import { SensemakerStore, sensemakerStoreContext } from "@neighbourhoods/client";
-import { NHButton, NHComponent } from "@neighbourhoods/design-system-components";
+import { NHButton, NHComponent, NHSpinner } from "@neighbourhoods/design-system-components";
 import { LeaveNeighbourhood } from "../dialogs/leave-neighbourhood";
 import { provideWeGroupInfo } from "../../matrix-helpers";
 
 export class NeighbourhoodSettings extends NHComponent {
-
   @consume({ context: matrixContext , subscribe: true })
   @property({attribute: false})
   _matrixStore!: MatrixStore;
@@ -45,136 +43,63 @@ export class NeighbourhoodSettings extends NHComponent {
   @query("#leave-nh-dialog")
   _leaveGroupDialog!: LeaveNeighbourhood;
 
-  renderJoinErrorSnackbar() {
-    return html`
-      <mwc-snackbar
-        id="join-error-snackbar"
-        labelText="Joining failed! (See console for details)"
-      >
-      </mwc-snackbar>
-    `;
-  }
-
-  renderInstallingProgress() {
-    return html`
-      <mwc-snackbar
-        id="installing-progress"
-        timeoutMs="-1"
-        labelText="Installing..."
-      >
-      </mwc-snackbar>
-    `;
-  }
-
-  renderSuccessSnackbar() {
-    return html`
-      <mwc-snackbar
-        id="installation-success"
-        labelText="Installation successful"
-      ></mwc-snackbar>
-    `;
-  }
-
   toggleAppletDescription() {
     this._showAppletDescription = !this._showAppletDescription;
   }
 
-  async leaveGroup(appletInstanceId: EntryHash) {
-    (this.shadowRoot?.getElementById("installing-progress") as Snackbar).show();
-    await this._matrixStore
-      .joinApplet(this.weGroupId, appletInstanceId)
-      .then(() => {
-        (
-          this.shadowRoot?.getElementById("installing-progress") as Snackbar
-        ).close();
-        (
-          this.shadowRoot?.getElementById("installation-success") as Snackbar
-        ).show();
-        this.requestUpdate(); // to show the newly installed applet in case user is still on same page
-        this.dispatchEvent(
-          new CustomEvent("applet-installed", {
-            detail: { appletEntryHash: appletInstanceId, weGroupId: this.weGroupId },
-            composed: true,
-            bubbles: true,
-        })
-      );
-      })
-      .catch((e) => {
-        (
-          this.shadowRoot?.getElementById("installing-progress") as Snackbar
-        ).close();
-        (
-          this.shadowRoot?.getElementById("join-error-snackbar") as Snackbar
-        ).show();
-        console.log("Joining error:", e);
-      });
-  }
-
   renderContent() {
     return html`
-        <leave-nh-dialog id="leave-nh-dialog" .weGroupId=${this.weGroupId}></leave-nh-dialog>
+      <leave-nh-dialog id="leave-nh-dialog" .weGroupId=${this.weGroupId}></leave-nh-dialog>
 
-        <slot name="to-join">
-          <h3>Applets to Join</h3>
-          <hr />
-          <joinable-applet-instance-list></joinable-applet-instance-list>
-        </slot>
+      <slot name="to-join">
+        <h3>Applets to Join</h3>
+        <hr />
+        <joinable-applet-instance-list></joinable-applet-instance-list>
+      </slot>
 
-        <slot name="installed">
-          <h3>Installed Applets</h3>
-          <hr />
-          <applet-instance-status-list></applet-instance-status-list>
-        </slot>
+      <slot name="installed">
+        <h3>Installed Applets</h3>
+        <hr />
+        <applet-instance-status-list></applet-instance-status-list>
+      </slot>
 
-        <slot name="uninstalled">
-          <h3>Uninstalled Applets</h3>
-          <hr />
-          <uninstalled-applet-instance-list></uninstalled-applet-instance-list>
-        </slot>
+      <slot name="uninstalled">
+        <h3>Uninstalled Applets</h3>
+        <hr />
+        <uninstalled-applet-instance-list></uninstalled-applet-instance-list>
+      </slot>
 
-        <slot name="danger-zone">
-          <h3>Danger Zone</h3>
-          <hr />
+      <slot name="danger-zone">
+        <h3>Danger Zone</h3>
+        <hr />
 
-          <div style="display: flex; align-items: center;">
-            <p>Leave neighbourhood and delete all applets: </p>
-            <span style="flex: 1"></span>
-            <nh-button
-              .size=${"auto"}
-              .variant=${"danger"}
-              @click=${() => this._leaveGroupDialog.open()}
-            >Leave Neighbourhood</nh-button>
-          </div>
-        </slot>
+        <div style="display: flex; align-items: center;">
+          <p>Leave neighbourhood and delete all applets: </p>
+          <span style="flex: 1"></span>
+          <nh-button
+            .size=${"auto"}
+            .variant=${"danger"}
+            @click=${() => this._leaveGroupDialog.open()}
+          >Leave Neighbourhood</nh-button>
+        </div>
+      </slot>
     `;
   }
 
-
   render() {
     return !this._neighbourhoodInfo.value
-      ? html`
-        <div class="center-content" style="flex: 1; width: 100%; height: 100%;">
-          <mwc-circular-progress indeterminate></mwc-circular-progress>
-        </div>
-      `
-      : html`
-        ${this.renderContent()}
-      `
+      ? html`<nh-spinner type=${"page"}></nh-spinner>`
+      : this.renderContent()
   }
 
-  static get elementDefinitions() {
-    return {
-      "nh-button": NHButton,
-      "mwc-circular-progress": CircularProgress,
-      "mwc-snackbar": Snackbar,
-      "mwc-linear-progress": LinearProgress,
-      "leave-nh-dialog": LeaveNeighbourhood,
-      "joinable-applet-instance-list": JoinableAppletInstanceList,
-      "applet-instance-status-list": AppletInstanceStatusList,
-      "uninstalled-applet-instance-list": UninstalledAppletInstanceList,
-    };
+  static elementDefinitions = {
+    "nh-button": NHButton,
+    "nh-spinner": NHSpinner,
+    "leave-nh-dialog": LeaveNeighbourhood,
+    "joinable-applet-instance-list": JoinableAppletInstanceList,
+    "applet-instance-status-list": AppletInstanceStatusList,
+    "uninstalled-applet-instance-list": UninstalledAppletInstanceList,
   }
-
 
   static get styles() {
     return css`
@@ -199,5 +124,4 @@ export class NeighbourhoodSettings extends NHComponent {
     }
   `;
   }
-
 }
