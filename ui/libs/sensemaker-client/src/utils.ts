c56b77e994cd22a6b29784ce69ef3fd0bffd7725
Nicholas Stebbings <nickolarse@riseup.net>
2024-05-21 14:41:34 +1200

    filter assessments for getMyLatestAssessment

--- ui/libs/sensemaker-client/src/utils.ts
+++ ui/libs/sensemaker-client/src/utils.ts
@@ -3,6 +3,10 @@ import { Assessment } from "./assessment";
 
 export type Option<Inner> = Inner | null
 
+export function compareUint8Arrays(a: Uint8Array, b: Uint8Array): boolean {
+    let ret_val = a.byteLength == b.byteLength;
+    return ret_val && a.reduce((prev: boolean, aByte: number, index: number) => (prev && aByte == b[index]), ret_val);
+}
 
 export function getLatestAssessment(assessments: Assessment[], dimension_eh: EntryHashB64): Option<Assessment> {
     const assessmentOnDimension = assessments.filter(assessment => encodeHashToBase64(assessment.dimension_eh) === dimension_eh);
