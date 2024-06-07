314377c35a6968764607a08c97a59426d56e117e
Nicholas Stebbings <nickolarse@riseup.net>
2024-06-07 11:46:09 +0700

    add resource-def-list and update to have dropdown for default trays

--- ui/app/src/nh-config/lists/resource-def-list.ts
+++ ui/app/src/nh-config/lists/resource-def-list.ts
@@ -1,24 +1,45 @@
 import { html, css, PropertyValueMap } from "lit";
 import { property, state } from "lit/decorators.js";
 
-import { ResourceDef, SensemakerStore } from "@neighbourhoods/client";
+import { AssessmentTrayConfig, ResourceDef, SensemakerStore, serializeAsyncActions } from "@neighbourhoods/client";
 
 import NHButton from '@neighbourhoods/design-system-components/button';
+import NHSelect from '@neighbourhoods/design-system-components/input/select';
 import NHButtonGroup from '@neighbourhoods/design-system-components/button-group';
 import NHCard from '@neighbourhoods/design-system-components/card';
 import NHCardList from '@neighbourhoods/design-system-components/card-list';
 import NHComponent from '@neighbourhoods/design-system-components/ancestors/base';
-import { b64images } from "@neighbourhoods/design-system-styles";
+import { EntryRecord } from "@holochain-open-dev/utils";
+import { EntryHash } from "@holochain/client";
 
 export default class ResourceDefList extends NHComponent {
   @property() sensemakerStore!: SensemakerStore;
 
-  @state() private _resourceDefEntries: Array<ResourceDef>;
+  @state() private _resourceDefEntries: Array<ResourceDef & { resource_def_eh: EntryHash, tray_config_eh?: EntryHash }> = [];
 
   protected async firstUpdated(_changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>) {
     const result = await this.sensemakerStore.getResourceDefs()
-    console.log('result :>> ', result);
-    // this._resourceDefEntries = result;
+    const destructuredEntryRecords = result?.map((eR: EntryRecord<ResourceDef>) => ({...eR.entry, resource_def_eh: eR.entryHash}))
+    this._resourceDefEntries = destructuredEntryRecords || []
+  }
+
+  protected async updated(changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>) {
+    if(changedProperties.has("_resourceDefEntries") && this._resourceDefEntries.length > 0) {
+      try {
+        const fetchDefaultActions: Array<()=>Promise<EntryRecord<AssessmentTrayConfig> | null>> = [];
+        this._resourceDefEntries.forEach(rd => {
+          fetchDefaultActions.push(async() => {
+            const maybeDefaultTrayConfig = await this.sensemakerStore.getDefaultAssessmentTrayForResourceDef(rd.resource_def_eh)
+            console.log('maybeDefaultTrayConfig for ' + rd.resource_name + ':>> ', maybeDefaultTrayConfig);
+            return maybeDefaultTrayConfig
+          })
+        })
+        serializeAsyncActions(fetchDefaultActions);
+      } catch (e) {
+        console.error(e)
+      }
+      debugger;
+    }
   }
 
   render() {
@@ -36,23 +57,27 @@ export default class ResourceDefList extends NHComponent {
           ${
             this._resourceDefEntries.map((resourceDef: ResourceDef) => html`
               <nh-card
-                class="tight button"
                 .theme=${'dark'}
                 .textSize=${"md"}
-                .hasPrimaryAction=${false}
-                .heading=${resourceDef['resource_name']}
-                .subheading=${"Used in " + resourceDef['resource_name']}
+                .title=${resourceDef['resource_name']?.split("_").join(" ").toUpperCase()}
+                
               >
+                <nh-select
+                  .errored=${false}
+                  .size=${"md"}
+                  .required=${false}
+                  .id=${resourceDef + "-set-tray-default"}
+                  .name=${resourceDef + "-set-tray-default"}
+                  .placeholder=${"Set default tray config"}
+                  .label=${"Pick default tray:"}
+                  @change=${(e: Event) => {}}
+                  .options=${[]}
+                >
+                </nh-select>
               </nh-card>
             `)
           }
         </nh-card-list>
-        <nh-button
-          variant=${"primary"}
-          iconImageB64=${b64images.icons.plus}
-          size=${"icon"}
-          slot="actions"
-        ></nh-button>
       </nh-button-group>
     `;
   }
@@ -60,6 +85,7 @@ export default class ResourceDefList extends NHComponent {
 
   static elementDefinitions = {
     "nh-button": NHButton,
+    "nh-select": NHSelect,
     "nh-button-group": NHButtonGroup,
     "nh-card": NHCard,
     "nh-card-list": NHCardList
