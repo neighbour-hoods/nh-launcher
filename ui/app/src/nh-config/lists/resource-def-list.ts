a46e0efc1018be98aeab3b6161829ffce4836d46
Nicholas Stebbings <nickolarse@riseup.net>
2024-06-18 11:43:31 +0700

    setting default assessment tray config entry for resource def

--- ui/app/src/nh-config/lists/resource-def-list.ts
+++ ui/app/src/nh-config/lists/resource-def-list.ts
@@ -15,12 +15,30 @@ import { EntryHash } from "@holochain/client";
 export default class ResourceDefList extends NHComponent {
   @property() sensemakerStore!: SensemakerStore;
 
-  @state() private _resourceDefEntries: Array<ResourceDef & { resource_def_eh: EntryHash, tray_config_eh?: EntryHash }> = [];
+  @state() private _resourceDefEntries: Array<ResourceDef & { resource_def_eh: EntryHash }> = [];
+
+  @state() private _assessmentTrayEntries!: Array<AssessmentTrayConfig & { assessment_tray_eh: EntryHash }>;
+
+  async fetchAssessmentTrayEntries() {
+    try {
+      const entryRecords = await this.sensemakerStore.getAssessmentTrayConfigs();
+      this._assessmentTrayEntries = entryRecords.map(entryRecord => {
+        return {
+          ...entryRecord.entry,
+          assessment_tray_eh: entryRecord.entryHash
+        }
+      })
+    } catch (error) {
+      console.log('Error fetching assessment trays: ', error);
+    }
+  }
 
   protected async firstUpdated(_changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>) {
     const result = await this.sensemakerStore.getResourceDefs()
     const destructuredEntryRecords = result?.map((eR: EntryRecord<ResourceDef>) => ({...eR.entry, resource_def_eh: eR.entryHash}))
     this._resourceDefEntries = destructuredEntryRecords || []
+
+    await this.fetchAssessmentTrayEntries();
   }
 
   protected async updated(changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>) {
@@ -54,12 +72,11 @@ export default class ResourceDefList extends NHComponent {
           .direction=${"vertical"}
         >
           ${
-            this._resourceDefEntries.map((resourceDef: ResourceDef) => html`
+            this._resourceDefEntries.map((resourceDef: ResourceDef & { resource_def_eh: EntryHash }) => html`
               <nh-card
                 .theme=${'dark'}
                 .textSize=${"md"}
                 .title=${resourceDef['resource_name']?.split("_").join(" ").toUpperCase()}
-                
               >
                 <nh-select
                   .errored=${false}
@@ -69,8 +86,34 @@ export default class ResourceDefList extends NHComponent {
                   .name=${resourceDef + "-set-tray-default"}
                   .placeholder=${"Set default tray config"}
                   .label=${"Pick default tray:"}
-                  @change=${(e: Event) => {}}
-                  .options=${[]}
+                  @change=${async(e: Event) => {
+                    const defaultTrayEntryHash = (e.target as any)?.value;
+                    if(!defaultTrayEntryHash) return;
+
+                    try {
+                      const setDefaultResult = await this.sensemakerStore.setDefaultAssessmentTrayForResourceDef(resourceDef.resource_def_eh, defaultTrayEntryHash);
+                      console.log('setDefaultResult :>> ', setDefaultResult);
+                      await this.updateComplete;
+                      this.dispatchEvent(
+                        new CustomEvent("trigger-alert", {
+                          detail: { 
+                            title: "Default Tray Updated",
+                            msg: "You have linked the Resource Definition to a default Assessment Tray configuration",
+                            type: "success",
+                            closable: true,
+                          },
+                          bubbles: true,
+                          composed: true,
+                        })
+                      );
+                    } catch (error) {
+                      console.log("Failed to set default tray for resource def: ", error)
+                    }
+                  }}
+                  .options=${this._assessmentTrayEntries?.map(trayEntry => ({
+                      label: trayEntry.name,
+                      value: trayEntry.assessment_tray_eh
+                  }))}
                 >
                 </nh-select>
               </nh-card>
